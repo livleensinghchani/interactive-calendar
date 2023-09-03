9f624a5abbeeeba1936fe22b98af32c44a0c2179
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-09-03 22:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 var tagCurrentDate = document.querySelector(".currentDate");
 var tagDayList = document.querySelector(".days");
-var btnControlIcon = document.querySelectorAll(".icon span");
+var controlIcon = document.querySelectorAll(".icon span");
 var date = new Date();
 var currentYear = date.getFullYear();
 var currentMonth = date.getMonth();
 var aryMonth = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 var loadCalendar = function () {
+    var firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+    //console.log(firstDayOfMonth);
     var lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-    var tempDayNumber = "";
+    //console.log(lastDateOfMonth);
+    var lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay();
+    //console.log(lastDayOfMonth);
+    var lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate();
+    //console.log(lastDateOfLastMonth);
+    var dayTags = "";
+    for (var index = firstDayOfMonth; index > 0; index--) {
+        dayTags += '<li class = "inactive">' + (lastDateOfLastMonth - index + 1) + '</li>';
+    }
     for (var index = 1; index <= lastDateOfMonth; index++) {
-        tagDayList.innerHTML += "<li>" + index + "</li>";
+        var activeDay = index === date.getDate() && currentMonth === new Date().getMonth()
+            && currentYear === new Date().getFullYear() ? "active" : "";
+        dayTags += '<li class =' + (activeDay) + '>' + index + '</li>';
+    }
+    for (var index = lastDayOfMonth; index < 6; index++) {
+        dayTags += '<li class = "inactive">' + (index - lastDayOfMonth + 1) + '</li>';
     }
     tagCurrentDate.innerText = aryMonth[currentMonth] + " " + (currentYear);
+    tagDayList.innerHTML = dayTags;
 };
 loadCalendar();
-btnControlIcon.forEach(function (element) {
+controlIcon.forEach(function (element) {
     element.addEventListener("click", function () {
-        console.log(element);
+        currentMonth = element.id === "btnPrev" ? currentMonth - 1 : currentMonth + 1;
+        if (currentMonth < 0 || currentMonth > 11) {
+            date = new Date(currentYear, currentMonth);
+            currentYear = date.getFullYear();
+            currentMonth = date.getMonth();
+        }
+        else {
+            date = new Date();
+        }
+        loadCalendar();
     });
 });
