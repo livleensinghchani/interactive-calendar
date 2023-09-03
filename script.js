4f424aae36341c778bbd393c31ebe69411694611
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-09-03 20:47:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var currentMonth = date.getMonth();
 var aryMonth = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 var loadCalendar = function () {
+    var lastDateOfMonth = new Date(currentYear, currentMonth, 0).getDate();
+    console.log(lastDateOfMonth);
     currentDate.innerText = aryMonth[currentMonth] + " " + (currentYear);
 };
 loadCalendar();
